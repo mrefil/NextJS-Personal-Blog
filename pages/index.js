1c41e46a1424d9da953e82614ee235a43e9c2d66
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-05 17:20:32 -0500

    Hero Component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
 
 function HomePage() {
   return (
