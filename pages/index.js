7ceec4212bccbb4f5f1430e79b435d9bee66da21
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-05 17:04:29 -0500

    Start Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function HomePage() {
+import { Fragment } from "react";
 
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  );
 }
 
 export default HomePage;
+
+// 1) Hero Section      => Present Ourselves
+// 2) Featured Posts
