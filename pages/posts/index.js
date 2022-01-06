25008ca3ffed92be7f60340f2ea2048a1134395d
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-05 19:43:05 -0500

    All Posts Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,40 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
 
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-nextjs.png', 
+        excerpt: 'NextJS is a the React Framework for production.', 
+        date: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs1', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-nextjs.png', 
+        excerpt: 'NextJS is a the React Framework for production.', 
+        date: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs2', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-with-nextjs.png', 
+        excerpt: 'NextJS is a the React Framework for production.', 
+        date: '2022-02-10',
+    },
+    {
+        slug: 'getting-started-with-nextjs3', 
+        title: 'Getting Started with NextJS', 
+        image: 'getting-started-with-nextjs.png', 
+        excerpt: 'NextJS is a the React Framework for production.', 
+        date: '2022-02-10',
+    },
+];
+
+function AllPostsPage() {
+    return (
+        <AllPosts posts={DUMMY_POSTS} />
+    )
 }
 
 export default AllPostsPage;
