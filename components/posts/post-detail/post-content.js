b0d4ea02a83f4f31bf67e7ff1cbc14d963465030
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-05 23:07:40 -0500

    Rendering Code Snippets From Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
 
 function PostContent(props) {
     const {post} = props;
@@ -37,7 +39,19 @@ function PostContent(props) {
             return (
                 <p>{paragraph.children}</p>
             );
-        }
+        },
+        code(code) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            
+            return (
+              <SyntaxHighlighter
+                style={atomDark}
+                language={language}
+                children={children}
+              />
+            );
+          },
     };
 
     return (
