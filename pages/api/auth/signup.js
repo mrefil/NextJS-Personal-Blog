3610f7fb19d89746acb7774da33fc4c42768164e
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-08 22:14:59 -0500

    Sending Signup Requests From The Frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,19 +1,22 @@
-import { hashPassword } from "../../../lib/auth";
+import { hashPassword } from '../../../lib/auth';
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return;
+    }
     const data = req.body;
     const { email, password} = data;
-    if (!email || !password || password.tirm().length < 7) {
+    if (!email || !password || password.trim().length < 7) {
         res.status(422).json({message: 'Invalid input - password should also be at least 7 characters long.'});
         return;
     }
     const client = await connectToDatabase();
     const db = client.db();
-    const hashPassword = hashPassword(password);
+    const hashedPassword = await hashPassword(password);
     const result = await db.collection('users').insertOne({
         email: email,
-        password: password
+        password: hashedPassword
     });
 
     res.status(201).json({message: 'Created user!'});
