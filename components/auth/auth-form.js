8fd0b112db9e92f47a7ad2c6502599210902a1b0
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-08 22:18:12 -0500

    Sending a "Signin" Request From The Frontend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import { signIn } from 'next-auth/client';
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -35,6 +36,16 @@ function AuthForm() {
 
     if (isLogin) {
       // log user in
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword
+      });
+
+      if(!result.error) {
+        // set some auth state
+      }
+      
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
